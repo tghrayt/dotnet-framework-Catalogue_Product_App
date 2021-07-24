99af31b49fd0c842adfda04f427b6ff702d6ba0a
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2021-07-25 00:36:50 +0100

    la création des vue produit et catalogue

---
+++ Controllers/CatalogueController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catalogue_Produit_App.Controllers
+{
+    public class CatalogueController : Controller
+    {
+        // GET: Catalogue
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AjoutCatalogue()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
---
+++ Controllers/ProduitController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catalogue_Produit_App.Controllers
+{
+    public class ProduitController : Controller
+    {
+        // GET: Produit
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AjoutProduit()
+        {
+            try
+            {
+                return View();
+            }catch(Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}

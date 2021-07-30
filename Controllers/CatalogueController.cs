fbd7bf33fcb44b31913996e987209728e71d832d
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2021-07-30 21:07:42 +0100

    Développement de module gestion des catégories

--- Controllers/CatalogueController.cs
+++ Controllers/CatalogueController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Catalogue_Produit_App.Models;
 
 namespace Catalogue_Produit_App.Controllers
 {
     public class CatalogueController : Controller
+       
     {
+        CatalogueProduitEntities db = new CatalogueProduitEntities();
         // GET: Catalogue
         public ActionResult Index()
         {
@@ -18,6 +22,7 @@ namespace Catalogue_Produit_App.Controllers
         {
             try
             {
+                ViewBag.listeCategorie = db.CAT_CATEGORIE.ToList();
                 return View();
             }
             catch (Exception e)
@@ -25,5 +30,83 @@ namespace Catalogue_Produit_App.Controllers
                 return HttpNotFound();
             }
         }
+
+        [HttpPost]
+        public ActionResult AjoutCatalogue(CAT_CATEGORIE categorie)//enregistrement
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    categorie.DATE_SAISIE = DateTime.Now;
+                    db.CAT_CATEGORIE.Add(categorie);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("AjoutCatalogue");
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        public ActionResult SupprimerCatalogue(int id)
+        {
+            try
+            {
+                CAT_CATEGORIE categorie = db.CAT_CATEGORIE.Find(id); //recherchede la categorie
+                if (categorie != null)
+                {
+                    db.CAT_CATEGORIE.Remove(categorie); //supprimer la categorie
+                    db.SaveChanges();//enregistrer le resultat
+                }
+                return RedirectToAction("AjoutCatalogue");
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        public ActionResult ModifierCatalogue(int id)
+        {
+            try
+            {
+                ViewBag.listeCategorie = db.CAT_CATEGORIE.ToList();
+                CAT_CATEGORIE categorie = db.CAT_CATEGORIE.Find(id);
+                if (categorie != null)
+                {
+                    return View("AjoutCatalogue", categorie);
+                }
+                return RedirectToAction("AjoutCatalogue");
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ModifierCatalogue(CAT_CATEGORIE categorie)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    
+                    db.Entry(categorie).State = EntityState.Modified; // modification de notre categorie
+                    db.SaveChanges();
+                }
+                return RedirectToAction("AjoutCatalogue");
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+
     }
 }
